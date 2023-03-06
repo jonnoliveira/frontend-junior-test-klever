9bc31bfb150f63607721f4bb7c05fda66fd0749f
Jonathas Oliveira <jonnutri@gmail.com>
2023-03-06 01:36:53 -0300

    add: 404 page

--- klever-code-app/src/pages/NotFound.jsx
+++ klever-code-app/src/pages/NotFound.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import error from '../assets/error.png';
+import '../css/notfound.css';
 
-function NotFound() {
+function NotFound({ history }) {
   return (
-    <div>NotFound</div>
+    <section className='notfound-container'>
+      <div className='notfound-image-coments'>
+        <img src={ error } alt="Error icon" />
+        <div className='notfound-coments'>
+          <h3>HM...</h3>
+          <p>Parece que erramos o caminho, não é mesmo? Tente novamente!</p>
+        </div>
+        <button
+          type='button'
+          onClick={() => history.push('/')}
+          className='notfound-btn'
+        >
+          Inicio
+        </button>
+      </div>
+    </section>
   )
 }
 
